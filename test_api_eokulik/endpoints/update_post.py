f4b0f86f065e4387d64c3a946012708be365a552
Summerduck <daria.samardak@gmail.com>
2024-10-28 15:10:46 +0200

    Update

--- test_api_eokulik/endpoints/update_post.py
+++ test_api_eokulik/endpoints/update_post.py
@@ -6,13 +6,13 @@ from test_api_eokulik.endpoints.endpoint import Endpoint
 
 class UpdatePost(Endpoint):
 
-    @allure.step("Update a post")
+    @allure.step('Update a post')
     def make_changes_in_post(self, post_id, payload, headers=None):
         headers = headers if headers else self.headers
         self.response = requests.put(
-            f"{self.url}/{post_id}",
+            f'{self.url}/{post_id}',
             json=payload,
-            headers=headers,
+            headers=headers
         )
         self.json = self.response.json()
         return self.response
