92a96fbb9a6d2d41900197a93cc930b4aa0100ee
Summerduck <daria.samardak@gmail.com>
2024-10-28 15:09:34 +0200

    Update

--- test_api_eokulik/endpoints/update_post.py
+++ test_api_eokulik/endpoints/update_post.py
@@ -10,7 +10,9 @@ class UpdatePost(Endpoint):
     def make_changes_in_post(self, post_id, payload, headers=None):
         headers = headers if headers else self.headers
         self.response = requests.put(
-            f"{self.url}/{post_id}", json=payload, headers=headers
+            f"{self.url}/{post_id}",
+            json=payload,
+            headers=headers,
         )
         self.json = self.response.json()
         return self.response
