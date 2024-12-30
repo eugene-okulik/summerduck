35293c2c5dabb4f16571b708c0c109ab9b034d4e
Summerduck <daria.samardak@gmail.com>
2024-12-30 11:30:31 +0200

    Enhance API request tests with Allure annotations for better reporting and tracking

--- test_api_summerduck/tests/test_api_requests.py
+++ test_api_summerduck/tests/test_api_requests.py
@@ -1,11 +1,18 @@
 import pytest
+import allure
 from test_api_summerduck.data import api_requests
 
 
 @pytest.mark.usefixtures("start_testing", "log_test_info")
+@allure.feature("API requests")
 class TestApiRequests:
 
     @pytest.mark.critical
+    @allure.testcase("QAP-001")
+    @allure.title("Get all objects")
+    @allure.tag("GET")
+    @allure.story("Retrieve all objects")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_get_all_objects(self, api_client):
         api_client.get_all_objects()
         api_client.check_that_status_is_200()
@@ -17,6 +24,11 @@ class TestApiRequests:
         indirect=["object_id"],
     )
     @pytest.mark.medium
+    @allure.testcase("QAP-002")
+    @allure.title("Get object by ID")
+    @allure.tag("GET")
+    @allure.story("Retrieve object by ID")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_get_object_by_id(self, api_client, object_id, expected_status_code):
         api_client.get_object_by_id(object_id)
         api_client.check_that_status_is(expected_status_code)
@@ -27,6 +39,11 @@ class TestApiRequests:
         api_requests.post_object_params,
         indirect=["data"],
     )
+    @allure.testcase("QAP-003")
+    @allure.title("Post object")
+    @allure.tag("POST")
+    @allure.story("Create a new object")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_post_object(self, api_client, data, name, expected_status_code):
         api_client.post_object(name, data)
         api_client.check_that_status_is(expected_status_code)
@@ -36,6 +53,11 @@ class TestApiRequests:
         api_requests.put_object_by_id_params,
         indirect=["object_id", "data"],
     )
+    @allure.testcase("QAP-004")
+    @allure.title("Put object by ID")
+    @allure.tag("PUT")
+    @allure.story("Update an object by ID")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_put_object_by_id(
         self, api_client, object_id, data, name, expected_status_code
     ):
@@ -48,12 +70,22 @@ class TestApiRequests:
         api_requests.patch_object_by_id_params,
         indirect=["object_id", "data"],
     )
+    @allure.testcase("QAP-005")
+    @allure.title("Patch object by ID")
+    @allure.tag("PATCH")
+    @allure.story("Partially update an object by ID")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_patch_object_by_id(
         self, api_client, object_id, data, name, expected_status_code
     ):
         api_client.patch_object_by_id(object_id, data, name)
         api_client.check_that_status_is(expected_status_code)
 
+    @allure.testcase("QAP-006")
+    @allure.title("Delete object by ID")
+    @allure.tag("DELETE")
+    @allure.story("Delete an object by ID")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_delete_object(self, api_client, created_object):
         api_client.delete_object_by_id(created_object)
         api_client.check_that_status_is_200()
