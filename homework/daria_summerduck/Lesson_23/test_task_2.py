93d80867fc90c4ea04eb519ab1135a251949bff8
Summerduck <daria.samardak@gmail.com>
2025-03-05 17:57:55 +0200

    update task 2

--- homework/daria_summerduck/Lesson_23/test_task_2.py
+++ homework/daria_summerduck/Lesson_23/test_task_2.py
@@ -58,10 +58,6 @@ def test_fill_form(
     gender = driver.find_element(By.XPATH, f"//label[text()='{gender_value}']")
     number = driver.find_element(By.ID, "userNumber")
     date_of_birth = driver.find_element(By.ID, "dateOfBirthInput")
-    year_dropdown = driver.find_element(
-        By.XPATH, '//select[@class="react-datepicker__year-select"]'
-    )
-
     subjects = driver.find_element(By.ID, "subjectsInput")
     hobbies = driver.find_element(By.XPATH, f"//label[text()='{hobbies_value}']")
     current_address = driver.find_element(By.ID, "currentAddress")
@@ -78,28 +74,32 @@ def test_fill_form(
 
     # Date of birth
     date_of_birth.click()
-    year_dropdown.is_displayed()
+    # wait for the year dropdown to be clickable
+    year_dropdown = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//select[@class="react-datepicker__year-select"]')
+        )
+    )
     year_dropdown.click()
-    year = driver.find_element(
+    year_option = driver.find_element(
         By.XPATH,
         f'//select[@class="react-datepicker__year-select"]//option[@value="{year_value}"]',
     )
-    year.click()
-    year_dropdown.click()
-    month = driver.find_element(
-        By.XPATH,
-        f'//select[@class="react-datepicker__month-select"]//option'
-        f'[text()="{month_of_birth_value}"]',
-    )
-
+    year_option.click()
+    # Select month
     month_dropdown = driver.find_element(
         By.XPATH, '//select[@class="react-datepicker__month-select"]'
     )
     month_dropdown.click()
-    month.click()
-    month_dropdown.click()
+    month_option = driver.find_element(
+        By.XPATH,
+        f'//select[@class="react-datepicker__month-select"]//option[text()="{month_of_birth_value}"]',
+    )
+    month_option.click()
+    # Select day (assuming day_value can be used directly; if needed, remove any leading 0)
+    day_int = str(int(day_value))
     day = driver.find_element(
-        By.XPATH, f'//div[contains(@aria-label,"{month_of_birth_value} {day_value}")]'
+        By.XPATH, f'//div[contains(@aria-label,"{month_of_birth_value} {day_int}")]'
     )
     day.click()
 
@@ -125,21 +125,3 @@ def test_fill_form(
     # Get the modal content and print it
     modal_content = driver.find_element(By.CLASS_NAME, "modal-body").text
     print(modal_content)
-
-    # Check the modal content
-    expected_modal_content = f"""
-        Student Name {first_name_value} {last_name_value}
-        Student Email {email_value}
-        Gender {gender_value}
-        Mobile {number_value}
-        Date of Birth {date_of_birth_value}
-        Subjects {subjects_value}
-        Hobbies {hobbies_value}
-        Picture
-        Address {current_address_value}
-        State and City {state_and_city_value}
-    """
-    print(expected_modal_content)
-    # assert (
-    #     modal_content == expected_modal_content
-    # ), "Expected modal content is not equal to the actual one"
