d755e6688d6e8c136d694e7d8290b27582fc9c01
Summerduck <daria.samardak@gmail.com>
2025-03-05 18:45:35 +0200

    fix linter issues

--- homework/daria_summerduck/Lesson_23/test_task_3.py
+++ homework/daria_summerduck/Lesson_23/test_task_3.py
@@ -1,14 +1,14 @@
 """
 Часть 3
 № 1
-Напишите тест, который заходит на страницу 
-https://www.qa-practice.com/elements/select/single_select, 
-выбирает какой-нибудь вариант из Choose language, кликает Submit и проверяет, 
-что в окошке с результатом отображается тот вариант, который был выбран. 
+Напишите тест, который заходит на страницу
+https://www.qa-practice.com/elements/select/single_select,
+выбирает какой-нибудь вариант из Choose language, кликает Submit и проверяет,
+что в окошке с результатом отображается тот вариант, который был выбран.
 
 № 2
-Напишите тест, который зайдет на страницу 
-https://the-internet.herokuapp.com/dynamic_loading/2, 
+Напишите тест, который зайдет на страницу
+https://the-internet.herokuapp.com/dynamic_loading/2,
 нажмет Start, и проверит, что на странице появляется текст "Hello World!"
 """
 
